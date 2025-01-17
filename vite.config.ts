e29223fcae4a772aed02543add210036ef584d73
iliasabai <iliasabaimain@gmail.com>
2025-01-17 22:26:52 +0500

    change configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // Это позволяет прослушивать все IP
+    port: 5555,      // Оставьте порт, используемый сервером
+  },
 })
